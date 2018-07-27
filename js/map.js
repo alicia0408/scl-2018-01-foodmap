1cef5a970dd648dad52329ec38a512933fb3c9c1
Alicia Centeno <alicent.08.02@gmail.com>
2018-07-26 21:11:02 -0400

    se logra ver data en consola dese google map

--- js/map.js
+++ js/map.js
@@ -1,66 +1,62 @@
+
+
 var map;
 var infowindow;
 
-function initMap() {
-	// Creamos un mapa con las coordenadas actuales
-	navigator.geolocation.getCurrentPosition(function (pos) {
+//CurrentPosition = ubicación 
+navigator.geolocation.getCurrentPosition(initMap);
 
-		lat = pos.coords.latitude;
-		lon = pos.coords.longitude;
+function initMap(position) {
+	//latitud y longitud de geolocalización
+	var lat = position.coords.latitude;
+	var lng = position.coords.longitude
+	//Obtener latitud y longitud de Santiago
+	var santiago = { lat, lng };
+	//Predeterminado
+	map = new google.maps.Map(document.getElementById('map'), {
+		center: { lat, lng },
+		zoom: 15
+	});
 
-		var myLatlng = new google.maps.LatLng(lat, lon);
+	infowindow = new google.maps.InfoWindow();
+	//Variable de PlaceService y enviamos la petición
+	var service = new google.maps.places.PlacesService(map);
+	service.nearbySearch({
+		//Rango de lugar, hasta dónde muestra restaurantes
+		location: santiago,
+		radius: 1000,
+		//Búsqueda de restaurant
+		type: ['restaurant']
+	}, callback);
 
-		var mapOptions = {
-			center: myLatlng,
-			zoom: 14,
-			mapTypeId: google.maps.MapTypeId.SATELLITE
-		};
 
-		map = new google.maps.Map(document.getElementById("mapa"), mapOptions);
+	function callback(results, status) {
 
-		// Creamos el infowindow
-		infowindow = new google.maps.InfoWindow();
-
-		// Especificamos la localización, el radio y el tipo de lugares que queremos obtener
-		var request = {
-			location: myLatlng,
-			radius: 5000,
-			types: ['restaurant']
-		};
-
-		// Creamos el servicio PlaceService y enviamos la petición.
-		var service = new google.maps.places.PlacesService(map);
-
-		service.nearbySearch(request, function (results, status) {
-			if (status === google.maps.places.PlacesServiceStatus.OK) {
-				for (var i = 0; i < results.length; i++) {
-					crearMarcador(results[i]);
-				}
+		if (status === google.maps.places.PlacesServiceStatus.OK) {
+			for (var i = 0; i < results.length; i++) {
+				createMarker(results[i]);
+				console.log(results);
 			}
-		});
-	});
-}
 
-function crearMarcador(place) {
-	// Creamos un marcador
-	var marker = new google.maps.Marker({
-		map: map,
-		position: place.geometry.location
-	});
-
-
-	// Asignamos el evento click del marcador
-	google.maps.event.addListener(marker, 'click', function () {
-		infowindow.setContent(place.name);
-		infowindow.open(map, this);
-	});
-}
-
-var service = new google.maps.places.PlacesService(map);
-service.getDetails({
-	placeId: place.place_id
-}, function (placeDetails, status) {
-	if (status == google.maps.places.PlacesServiceStatus.OK) {
-		alert('placeDetails.formatted_address');
+		}
 	}
-});
+
+	//Para añadir marcador
+	function createMarker(place) {
+
+		var loCplace = place.geometry.location;
+		var marker = new google.maps.Marker({
+			map: map,
+			position: place.geometry.location
+		});
+
+		//Click del marcador
+		google.maps.event.addListener(marker, 'click', function () {
+			infowindow.setContent(place.name);
+
+			infowindow.open(map, this);
+
+		});
+	}
+
+}
